70a9976c05931b332a85553ee1e2f6dd8a0d41cd
Harold Zhac <developer@creatiweb.ca>
2020-10-17 21:56:38 -0400

    chore: Add Google analytics and Tag manager

--- gatsby-config.js
+++ gatsby-config.js
@@ -34,6 +34,58 @@ module.exports = {
     },
     'gatsby-plugin-netlify-cache',
     `gatsby-plugin-sitemap`,
+    {
+      resolve: "gatsby-plugin-google-tagmanager",
+      options: {
+        id: process.env.GATSBY_GOOGLE_TAGMANAGER_ID,
+  
+        // Include GTM in development.
+        //
+        // Defaults to false meaning GTM will only be loaded in production.
+        includeInDevelopment: false,
 
+        // datalayer to be set before GTM is loaded
+        // should be a stringified object or object
+        //
+        // Defaults to null
+        defaultDataLayer: function () {
+          return {
+            pageType: window.pageType,
+          }
+        },  
+
+        // Name of the event that is triggered
+        // on every Gatsby route change.
+        //
+        // Defaults to gatsby-route-change
+        routeChangeEventName: process.env.GATSBY_ROUTE_CHANGE,
+      }
+    },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        // The property ID; the tracking code won't be generated without it
+        trackingId: process.env.GATSBY_GOOGLE_ANALYTICS_TRACKING_ID,
+        // Defines where to place the tracking script - `true` in the head and `false` in the body
+        head: false,
+        // Setting this parameter is optional
+        anonymize: true,
+        // Setting this parameter is also optional
+        respectDNT: true,
+        // Avoids sending pageview hits from custom paths
+        exclude: ["/preview/**"],
+        // Delays sending pageview hits on route update (in milliseconds)
+        pageTransitionDelay: 0,
+        // Enables Google Optimize using your container Id
+        optimizeId: process.env.GATSBY_GOOGLE_OPTIMIZE_TRACKING_ID,
+        // Enables Google Optimize Experiment ID
+        experimentId: process.env.GATSBY_GOOGLE_EXPERIMENT_ID,
+        defer: false,
+        // Any additional optional fields
+        sampleRate: 5,
+        siteSpeedSampleRate: 10,
+        cookieDomain: "creatiweb.ca",
+      },
+    },    
   ],
 };
